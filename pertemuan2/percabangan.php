33e7123da1e7210e73c8407d81cf090b0d47d043
Ahmad Widhi Praditya <widhipraditya@gmail.com>
2026-08-03 10:50:28 +0700

    percabangan

---
+++ pertemuan2/percabangan.php
@@ -0,0 +1,15 @@
+<?php
+$nilai = readline("Masukkan nilai: ");
+
+if ($nilai >= 90) {
+    echo "A\n";
+} 
+elseif ($nilai >= 80) {
+    echo "B\n";
+} 
+elseif ($nilai >= 70) {
+    echo "C\n";
+} 
+else {
+    echo "D\n";
+}

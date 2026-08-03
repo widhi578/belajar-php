4adc4d372b29e7c34d486bc1b4e6c1734dcd8dd4
Ahmad Widhi Praditya <widhipraditya@gmail.com>
2026-08-03 11:52:51 +0700

    array

---
+++ pertemuan2/array.php
@@ -0,0 +1,13 @@
+<?php 
+
+// $buah = ["apel", "jeruk", "mangga", "pisang"];
+// $buah[] = "anggur"; // menambahkan data ke array
+// array_push($buah, "semangka"); // cara lain menambahkan data ke array
+// count($buah); // menghitung jumlah data di array
+// print_r($buah); // menampilkan isi array
+
+$siswa = ["nama" => "Nama  : Rina",
+          "nilai" => "Nilai : 88",];
+
+echo $siswa["nama"] . "\n";
+echo $siswa["nilai"] . "\n";
